9f6c353dc58d845e81dbbbaf69bbe5f5403d84fa
Igor Guerrero <igorgue@protonmail.com>
2025-11-02 00:29:24 -0600

    feat: add midi patterns and reorganize sample structure
    - Add midi.tidal with drum patterns and MIDI examples
    - Move samples from Dirt/samples to samples directory
    - Update BootSuperDirt.scd to use new sample path
    - Add hush to intro.tidal
    - Configure vim settings in .lazy.lua

--- .lazy.lua
+++ .lazy.lua
@@ -1,6 +1,8 @@
-vim.g.focus_mode = false
 vim.opt.cursorline = true
 vim.opt.cursorcolumn = true
+
+vim.g.focus_mode = false
+vim.g.cmp_auto_brackets = false
 
 vim.defer_fn(function()
 	require("gitsigns").toggle_signs(false)
