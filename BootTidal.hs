fa2f2b59df83acb94ebece2bd8e13b286c42d359
Igor Guerrero <igorgue@protonmail.com>
2025-11-03 02:35:02 -0600

    feat: enable Link clock synchronization in TidalCycles configuration
    - Add cEnableLink = True to defaultConfig in BootTidal.hs
    - Update MIDI pattern in midi.tidal with new notes and remove hush

--- BootTidal.hs
+++ BootTidal.hs
@@ -8,7 +8,7 @@ default (Rational, Integer, Double, Pattern String)
 
 let editorTarget = Target {oName = "editor", oAddress = "127.0.0.1", oPort = 6013, oLatency = 0.03, oSchedule = Live, oWindow = Nothing, oHandshake = False, oBusPort = Nothing }
 let editorShape = OSCContext "/editor/highlights"
-tidalInst <- mkTidalWith [(superdirtTarget { oLatency = -0.02 }, [superdirtShape]), (editorTarget, [editorShape])] (defaultConfig {cFrameTimespan = 1/50, cProcessAhead = 1/20})
+tidalInst <- mkTidalWith [(superdirtTarget { oLatency = -0.02 }, [superdirtShape]), (editorTarget, [editorShape])] (defaultConfig {cEnableLink = True, cFrameTimespan = 1/50, cProcessAhead = 1/20})
 
 instance Tidally where tidal = tidalInst
 
